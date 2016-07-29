e26922b150d6deffb2577885174e86585f40ca1c
Anudeep <anudeep@Viveks-Mac-mini.local.lan>
2016-07-29 19:54:34 +0530

    updates

--- app/scripts/controllers/ForgotPasswdCtrl.js
+++ app/scripts/controllers/ForgotPasswdCtrl.js
@@ -1,10 +1,35 @@
 angular.module('clientApp')
-    .controller('ForgotPasswdCtrl', ['$scope', '$state', 'LoginService', 'SessionService', 'AlertService', function($scope, $state, LoginService, SessionService, AlertService) {
+    .controller('ForgotPasswdCtrl', ['$scope', '$state', 'LoginService', 'SessionService', 'AlertService', '$mdDialog', function($scope, $state, LoginService, SessionService, AlertService, $mdDialog) {
 
         $scope.user = {};
 
+        $scope.validateuser = function(ev) {
+            $mdDialog.show({
+                    controller: function($scope, $mdDialog, AlertService) {
+                        $scope.moduleObj = {
+                            "clientSecret": "nbosSecret"
+                        };
+                        $scope.create = function() {
+                            $mdDialog.hide(response);
+                        };
+
+                        $scope.closeDialog = function() {
+                            $mdDialog.cancel();
+                        }
+                    },
+                    templateUrl: 'app/views/OtpModal.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev
+                })
+                .then(function() {
+                   
+                }, function() {
+                    $scope.status = 'You cancelled the dialog.';
+                });
+        };
+
         var init = function() {
-        	
+
         }
         init();
     }]);
