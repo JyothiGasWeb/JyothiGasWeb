ea03aeab0da5be7340c062169f5b9f5c7e5b90ae
Anudeep <anudeep@Viveks-Mac-mini.local.lan>
2016-08-14 22:43:34 +0530

    updates

--- app/scripts/controllers/consumer/DealerChangeCtrl.js
+++ app/scripts/controllers/consumer/DealerChangeCtrl.js
@@ -4,12 +4,31 @@
  * Description
  */
 angular.module('clientApp').
-controller('DealerChangeCtrl', ['$scope', 'SessionService', function($scope, SessionService) {
+controller('DealerChangeCtrl', ['$scope', 'SessionService', 'RegisterService', function($scope, SessionService, RegisterService) {
 
     $scope.current = {};
+    $scope.user = {};
     var getCurrentAdd = function() {
-        $scope.current = SessionService.getConsumerSession().consumer;
+        RegisterService.getAllDealers().then(function(response) {
+            $scope.availableDealers = response;
+            for (var i = 0, len = $scope.availableDealers.length; i < len; i++) {
+                if (SessionService.getConsumerSession().consumer.dealerId == $scope.availableDealers[i].id) {
+                    $scope.current = $scope.availableDealers[i];
+                    break;
+                }
+            }
+        }, function(error) {
+            console.log("error getting dealers list");
+        })
+    };
+
+    $scope.reset = function(){
+    	$scope.user = {
+    		"newPinCode": "",
+    		"newAddress": ""
+    	};
     }
+
     var init = function() {
         getCurrentAdd();
     };
