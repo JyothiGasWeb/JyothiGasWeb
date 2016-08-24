183f591da553ea8c462b8e9ecd03fd3fb4094754
Anudeep <anudeepd@nbsotech.com>
2016-08-24 19:59:29 +0530

    ui updates

--- app/scripts/controllers/consumer/SearchDealerCtrl.js
+++ app/scripts/controllers/consumer/SearchDealerCtrl.js
@@ -4,21 +4,46 @@
  * Description
  */
 angular.module('clientApp').
-controller('SearchDealerCtrl', ['$scope', 'RegisterService', 'ConsumerService', 'AlertService', function($scope, RegisterService, ConsumerService, AlertService) {
+controller('SearchDealerCtrl', ['$scope', 'RegisterService', 'ConsumerService', 'AlertService', 'SessionService', 'LoginService', function($scope, RegisterService, ConsumerService, AlertService, SessionService, LoginService) {
 
-	$scope.showDealers = false;
+    $scope.showDealers = false;
+    var user = SessionService.getConsumerSession().consumer;
     var getAllDealers = function() {
         RegisterService.getAllDealers().then(function(response) {
             $scope.availableDealers = response;
-            console.log($scope.availableDealers)
         }, function(error) {
             console.log("error getting dealers list");
         })
     };
 
-    $scope.updateDealer = function(){
+    $scope.updateDealer = function(item) {
         //Need to integrate with API
-    }
+        var obj = {
+            "id": user.reg_id,
+            "dealerId": item.id
+        }
+        ConsumerService.updateDealer(obj).then(function(response) {
+            if (response.status == 'OK') {
+                updateUser();
+            }
+        }, function(error) {
+            AlertService.alert("Error Updating Dealer", 'md-warn');
+            console.log("error updating dealer");
+        })
+    };
+
+    var updateUser = function() {
+        var obj = {
+            "email": user.email
+        }
+        LoginService.getConsumer(obj).then(function(response) {
+            SessionService.setConsumerSession(response);
+            AlertService.alert("Dealer Updated Successfully", 'md-primary');
+            //$scope.reset();
+        }, function(response) {
+            console.log("errror");
+        });
+    };
 
     $scope.filterDealers = function() {
         $scope.dealers = [];
