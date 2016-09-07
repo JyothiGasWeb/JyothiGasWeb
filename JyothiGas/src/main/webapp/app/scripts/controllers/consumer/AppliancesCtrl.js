be6375a5229d9f53273dbfa13236b29a5429b6f6
Anudeep <anudeepd@nbsotech.com>
2016-09-07 20:21:17 +0530

    image changes

--- JyothiGas/src/main/webapp/app/scripts/controllers/consumer/AppliancesCtrl.js
+++ JyothiGas/src/main/webapp/app/scripts/controllers/consumer/AppliancesCtrl.js
@@ -4,12 +4,15 @@
  * Description
  */
 angular.module('clientApp').
-controller('AppliancesCtrl', ['$scope', 'ConsumerService', function($scope, ConsumerService) {
+controller('AppliancesCtrl', ['$scope', 'ConsumerService', '_', function($scope, ConsumerService, _) {
 
 
 	var getAppliances = function() {
         ConsumerService.getAppliances().then(function(response) {
             $scope.appliances = response;
+            _.each($scope.appliances, function(item){
+            	item.imgSrc = "app/images/"+ item.appliance_Name+".jpg";
+            });
         }, function(error) {
 
         })
--- JyothiGas/src/main/webapp/app/scripts/controllers/consumer/IndustrialCtrl.js
+++ JyothiGas/src/main/webapp/app/scripts/controllers/consumer/IndustrialCtrl.js
@@ -4,45 +4,48 @@
  * Description
  */
 angular.module('clientApp').
-controller('IndustrialCtrl', ['$scope', 'ConsumerService', 'SessionService', function($scope, ConsumerService, SessionService) {
+controller('IndustrialCtrl', ['$scope', 'ConsumerService', 'SessionService', '_', function($scope, ConsumerService, SessionService, _) {
 
     var user = SessionService.getConsumerSession().consumer;
     $scope.connection = {};
 
     var getProductType = function() {
-    	var obj = {
-    		"connectionTypeID": user.connectionTypeId
-    	}
+        var obj = {
+            "connectionTypeID": user.connectionTypeId
+        }
         ConsumerService.getProductType(obj).then(function(response) {
-        	$scope.products = response;
-        	$scope.connection.connectionType = response[0];
-        	$scope.calculate();
-        	var priceObj = {
-        		"gasRefill_charges": 200,
-        		"handling_charges": 300,
-        		"delievery_charges": 120
-        	};
-        	SessionService.createPriceSession(priceObj);
+            $scope.products = response;
+            $scope.connection.connectionType = response[0];
+            $scope.calculate();
+            var priceObj = {
+                "gasRefill_charges": 200,
+                "handling_charges": 300,
+                "delievery_charges": 120
+            };
+            SessionService.createPriceSession(priceObj);
             getAppliances();
         }, function(error) {
 
         })
     };
 
-    $scope.calculate = function(){
-    	$scope.total = 2000 + 400 + 200 + $scope.connection.connectionType.applliance_Cost;
+    $scope.calculate = function() {
+        $scope.total = 2000 + 400 + 200 + $scope.connection.connectionType.applliance_Cost;
     };
 
     var getAppliances = function() {
         ConsumerService.getAppliances().then(function(response) {
             $scope.appliances = response;
+            _.each($scope.appliances, function(item) {
+                item.imgSrc = "app/images/" + item.appliance_Name + ".jpg";
+            });
         }, function(error) {
 
         })
     }
 
     var init = function() {
-    	SessionService.deletePriceSession();
+        SessionService.deletePriceSession();
         getProductType();
     };
     init();
